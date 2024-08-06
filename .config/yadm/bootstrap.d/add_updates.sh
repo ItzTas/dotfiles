907031dd315167db073da97a895a271e71d075c2
ItzTas <talessabini2@gmail.com>
2024-08-06 02:24:46 -0300

    files updated

--- .config/yadm/bootstrap.d/add_updates.sh
+++ .config/yadm/bootstrap.d/add_updates.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -n "$(yadm status --porcelain)" ]; then
+	rm "$HOME"/.Brewfile
 	brew bundle dump --describe --file "$HOME"/.Brewfile
-	brewBundle
 	yadm commit -a -m "updated"
 	yadm push origin master
 fi
