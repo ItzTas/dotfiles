b5fa788c02c6bdf2e082e3c5a36104f4e40dddfb
ItzTas <talessabini2@gmail.com>
2024-08-06 02:31:45 -0300

    files updated

--- .config/yadm/bootstrap.d/add_updates.sh
+++ .config/yadm/bootstrap.d/add_updates.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -n "$(yadm status --porcelain)" ]; then
-	rm "$HOME"/.aptlis
+	rm "$HOME"/.aptlist
 	apt list --installed >"$HOME"/.aptlist
 	rm "$HOME"/.Brewfile
 	brew bundle dump --describe --file "$HOME"/.Brewfile
