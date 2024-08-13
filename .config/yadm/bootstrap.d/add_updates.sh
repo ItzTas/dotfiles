2daa530d6b81826dd1cae52c4b8433506790b439
ItzTas <talessabini2@gmail.com>
2024-08-12 20:54:12 -0400

    updated

--- .config/yadm/bootstrap.d/add_updates.sh
+++ .config/yadm/bootstrap.d/add_updates.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if [ -n "$(yadm status --porcelain)" ]; then
-	rm "$HOME"/.snaplist
-	snap list >"$HOME"/.snaplist
-	rm "$HOME"/.aptlist
-	apt list --installed >"$HOME"/.aptlist
-	rm "$HOME"/.Brewfile
-	brew bundle dump --describe --file "$HOME"/.Brewfile
-	yadm commit -a -m "updated"
-	yadm push origin master
-fi
+rm "$HOME"/.snaplist
+snap list >"$HOME"/.snaplist
+rm "$HOME"/.aptlist
+apt list --installed >"$HOME"/.aptlist
+rm "$HOME"/.Brewfile
+brew bundle dump --describe --file "$HOME"/.Brewfile
+yadm commit -a -m "updated"
+yadm push origin master
