a42fcf2d391798e126de99ae824e21b92a8c5396
ItzTas <talessabini2@gmail.com>
2024-09-17 01:45:17 -0400

    updated

--- .config/i3-resurrect/restore_sessions.sh
+++ .config/i3-resurrect/restore_sessions.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 i3-resurrect restore -w 1
+sleep 1
 i3-resurrect restore -w 2
+sleep 1
 i3-resurrect restore -w 3
+sleep 1
 i3-resurrect restore -w 4
+sleep 1
 i3-resurrect restore -w 5
+sleep 1
 i3-resurrect restore -w 6
+sleep 1
 i3-resurrect restore -w 7
+sleep 1
 i3-resurrect restore -w 8
+sleep 1
 i3-resurrect restore -w 9
+sleep 1
 i3-resurrect restore -w 0
-
