4b797661848f0b4874f686898096aa02319c44af
ItzTas <talessabini2@gmail.com>
2024-11-01 15:18:06 -0400

    updates

--- .config/hypr/scripts/screen_record.sh
+++ .config/hypr/scripts/screen_record.sh
@@ -1,23 +1,29 @@
+#!/bin/bash
+
 DIR=~/Videos/.recordings
 mkdir -p "$DIR"
 
 FILENAME="$DIR/recording_$(date '+%Y-%m-%d_%H-%M-%S').mp4"
 BASENAME=$(basename "$FILENAME")
 
-ACTION=$(dunstify "Gravação Iniciada" "Gravando para $FILENAME")
+dunstify "Recording Started" "Recording to $FILENAME" || exit 1
 
-wf-recorder -f "$FILENAME"
+if ! wf-recorder -f "$FILENAME"; then
+    dunstify "Error" "Failed to start recording" && exit 1
+fi
 
 THUMBNAIL="$DIR/thumbnail_$(date '+%Y-%m-%d_%H-%M-%S').png"
-ffmpeg -y -i "$FILENAME" -vframes 1 "$THUMBNAIL"
+if ! ffmpeg -y -i "$FILENAME" -vframes 1 "$THUMBNAIL"; then
+    dunstify "Error" "Failed to generate thumbnail" && rm "$FILENAME" && exit 1
+fi
 
-ACTION=$(dunstify -i "$THUMBNAIL" --action="default,VLC" "Gravação Terminada" "A gravação foi salva como $BASENAME")
+ACTION=$(dunstify -i "$THUMBNAIL" --action="default,VLC" "Recording Finished" "The recording has been saved as $BASENAME")
 
 rm "$THUMBNAIL"
 
 case "$ACTION" in
 "default")
-    dunstify "Abrindo $BASENAME com vlc"
+    dunstify "Opening $BASENAME with VLC"
     vlc "$FILENAME" &
     ;;
 esac
