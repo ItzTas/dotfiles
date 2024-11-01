5b7ca859d27fea16f5d82530a4cacb15d42236c5
ItzTas <talessabini2@gmail.com>
2024-11-01 15:09:49 -0400

    updated

--- .config/hypr/scripts/screen_record.sh
+++ .config/hypr/scripts/screen_record.sh
@@ -1,18 +1,23 @@
-#!/bin/bash
-
 DIR=~/Videos/.recordings
 mkdir -p "$DIR"
 
 FILENAME="$DIR/recording_$(date '+%Y-%m-%d_%H-%M-%S').mp4"
 BASENAME=$(basename "$FILENAME")
 
-notify-send -i "audio-input-microphone" "Gravação Iniciada" "Gravando para $FILENAME"
+ACTION=$(dunstify "Gravação Iniciada" "Gravando para $FILENAME")
 
 wf-recorder -f "$FILENAME"
 
 THUMBNAIL="$DIR/thumbnail_$(date '+%Y-%m-%d_%H-%M-%S').png"
 ffmpeg -y -i "$FILENAME" -vframes 1 "$THUMBNAIL"
 
-notify-send -i "$THUMBNAIL" "Gravação Terminada" "A gravação foi salva como $BASENAME"
+ACTION=$(dunstify -i "$THUMBNAIL" --action="default,VLC" "Gravação Terminada" "A gravação foi salva como $BASENAME")
 
 rm "$THUMBNAIL"
+
+case "$ACTION" in
+"default")
+    dunstify "Abrindo $BASENAME com vlc"
+    vlc "$FILENAME" &
+    ;;
+esac
