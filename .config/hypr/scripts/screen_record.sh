6b04491af065054d7aaef5a5019cebf1ee98b68f
ItzTas <talessabini2@gmail.com>
2024-11-01 14:03:05 -0400

    updates

--- .config/hypr/scripts/screen_record.sh
+++ .config/hypr/scripts/screen_record.sh
@@ -4,6 +4,7 @@ DIR=~/Videos/.recordings
 mkdir -p "$DIR"
 
 FILENAME="$DIR/recording_$(date '+%Y-%m-%d_%H-%M-%S').mp4"
+BASENAME=$(basename "$FILENAME")
 
 notify-send -i "audio-input-microphone" "Gravação Iniciada" "Gravando para $FILENAME"
 
@@ -12,6 +13,6 @@ wf-recorder -f "$FILENAME"
 THUMBNAIL="$DIR/thumbnail_$(date '+%Y-%m-%d_%H-%M-%S').png"
 ffmpeg -y -i "$FILENAME" -vframes 1 "$THUMBNAIL"
 
-notify-send -i "$THUMBNAIL" "Gravação Terminada" "A gravação foi salva como $FILENAME"
+notify-send -i "$THUMBNAIL" "Gravação Terminada" "A gravação foi salva como $BASENAME"
 
 rm "$THUMBNAIL"
