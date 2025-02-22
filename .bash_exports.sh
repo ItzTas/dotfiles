fd2b93f1df6076b21819ef944387091d0a0c1e28
ItzTas <talessabini2@gmail.com>
2025-02-22 17:52:01 -0400

    updates

--- .bash_exports.sh
+++ .bash_exports.sh
@@ -22,4 +22,14 @@ export HYPRCURSOR_THEME="Bibata-Modern-Classic"
 # hyprshot
 export HYPRSHOT_DIR="$HOME/Pictures/screenshots/"
 
+# XDG directories
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_TEMPLATES_DIR="$HOME/Templates"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_DOCUMENTS_DIR="$HOME/Docs"
+export XDG_MUSIC_DIR="$HOME/Music"
 export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_VIDEOS_DIR="$HOME/Videos"
