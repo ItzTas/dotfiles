a23d1e00a9949fa1e68220e5cf80c3f45cbc0b8e
ItzTas <talessabini2@gmail.com>
2025-02-22 17:40:15 -0400

    updates

---
+++ .bash_exports.sh
@@ -0,0 +1,14 @@
+export IGNOREEOF=999
+
+MY_PROJECT_PATH="$(pwd)"
+export MY_PROJECT_PATH
+
+# editor
+export EDITOR='nvim'
+
+# colors in gcc
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+export HISTSIZE=1000
+export HISTFILESIZE=2000
