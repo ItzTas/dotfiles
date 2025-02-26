273ace8966ca47ccfc13faab32ac97ed4eb70b56
ItzTas <talessabini2@gmail.com>
2025-02-25 21:33:30 -0400

    updates

---
+++ .config/hypr/scripts/print_area.sh
@@ -0,0 +1,22 @@
+print_selected_area() {
+    local filename
+    local path
+    local fullpath
+
+    filename="screenshot_$(date '+%Y-%m-%d_%H-%M-%S-%3N').png"
+    path="$(xdg-user-dir PICTURES)/screenshots"
+    fullpath="$path/$filename"
+
+    mkdir -p "$path"
+
+    grim -g "$(slurp)" "$fullpath"
+
+    local ACTION
+    ACTION=$(dunstify -i "$fullpath" --action="default,swappy" "Screenshot saved" "Image saved in $fullpath")
+
+    case "$ACTION" in
+    "default") swappy -f "$fullpath" ;;
+    esac
+}
+
+print_selected_area
