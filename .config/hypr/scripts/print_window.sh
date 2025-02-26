f1f7e0fc3adcc6b20bcfe2227cdd53de8b2a5c19
ItzTas <talessabini2@gmail.com>
2025-02-25 21:44:18 -0400

    updates

---
+++ .config/hypr/scripts/print_window.sh
@@ -0,0 +1,22 @@
+print_window() {
+    local filename
+    local path
+    local fullpath
+    local ACTION
+
+    filename="screenshot_$(date '+%Y-%m-%d_%H-%M-%S-%3N').png"
+    path="$(xdg-user-dir PICTURES)/screenshots"
+    fullpath="$path/$filename"
+
+    mkdir -p "$path"
+
+    hyprshot -m window -s -o "$path" -f "$filename"
+
+    ACTION=$(dunstify -i "$fullpath" --action="default,swappy" "Screenshot saved" "Image saved in $fullpath")
+
+    case "$ACTION" in
+    "default") swappy -f "$fullpath" ;;
+    esac
+}
+
+print_window
