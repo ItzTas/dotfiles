cdd74b4a953b51691236ceba9b3403b30b55d604
ItzTas <talessabini2@gmail.com>
2025-03-13 21:12:28 -0400

    updates

--- .bash_evals.sh
+++ .bash_evals.sh
@@ -2,7 +2,15 @@
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # oh my posh
-eval "$(oh-my-posh init bash --config "$HOME"/.config/ohmyposh/my_amro.toml)"
+if command -v oh-my-posh &>/dev/null; then
+    if [ -f "$HOME/.config/ohmyposh/my_amro.toml" ]; then
+        eval "$(oh-my-posh init bash --config "$HOME"/.config/ohmyposh/my_amro.toml)"
+    else
+        if command -v brew &>/dev/null && [ -f "$(brew --prefix oh-my-posh)/themes/amro.omp.json" ]; then
+            eval "$(oh-my-posh init zsh --config "$(brew --prefix oh-my-posh)/themes/amro.omp.json")"
+        fi
+    fi
+fi
 
 # zoxide
 eval "$(zoxide init --cmd cd bash)"
