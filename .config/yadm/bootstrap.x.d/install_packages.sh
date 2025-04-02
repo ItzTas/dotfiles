a467fca4e857f33a90741af6cb632544c5b43813
Tales Sabini <talessabini2@gmail.com>
2025-04-02 13:48:51 -0400

    updates

--- .config/yadm/bootstrap.x.d/install_packages.sh
+++ .config/yadm/bootstrap.x.d/install_packages.sh
@@ -1,103 +1,127 @@
 #!/bin/bash
 
-mkdir -p "$HOME/Downloads/"
+[ -d "$HOME/Downloads" ] || mkdir "$HOME/Downloads"
 
 cd "$HOME/Downloads/" || exit 1
 
 _install_yay() {
-    if command -v yay &>/dev/null; then
-        echo "yay is already installed. Skipping installation."
-        return
-    fi
+	set -e
+	if command -v yay &>/dev/null; then
+		echo "yay is already installed. Skipping installation."
+		return
+	fi
 
-    echo "Downloading and installing yay"
-    git clone https://aur.archlinux.org/yay.git
-    cd yay || exit 1
-    makepkg -si --noconfirm
-    cd ..
-    rm -rf yay
+	echo "Downloading and installing yay"
+	git clone https://aur.archlinux.org/yay.git
+	cd yay || exit 1
+	makepkg -si --noconfirm
+	cd ..
+	rm -rf yay
 }
 
 _install_pacman_packages() {
-    set -e
-    local packages=(
-        "cmake"
-        "ferdium"
-        "discord"
-        "dotnet-sdk"
-        "easyeffects"
-        "fd"
-        "fzf"
-        "sesh-bin"
-        "github-cli"
-        "glues"
-        "grim"
-        "hyprpaper"
-        "hyprshot"
-        "kitty"
-        "lazygit"
-        "lsd"
-        "man-db"
-        "nemo"
-        "neovim"
-        "oh-my-posh"
-        "pacfiles"
-        "ripgrep"
-        "imv"
-        "dunst"
-        "zen-browser-bin"
-        "bat"
-        "bash-language-server"
-        "rofi"
-        "shellcheck"
-        "shfmt"
-        "slurp"
-        "spotify"
-        "stacer-bin"
-        "termpicker"
-        "thunderbird"
-        "tmux"
-        "ttf-jetbrains-mono-nerd"
-        "unzip"
-        "waybar"
-        "wl-clipboard"
-        "xdg-user-dirs"
-        "yq"
-        "zip"
-        "zoxide"
-        "zsa-keymap-bin"
-        "sddm-sugar-candy-git"
-    )
+	set -e
+	local packages=(
+		"cmake"
+		"ferdium"
+		"discord"
+		"dotnet-sdk"
+		"easyeffects"
+		"fd"
+		"fzf"
+		"sesh-bin"
+		"github-cli"
+		"glues"
+		"grim"
+		"hyprpaper"
+		"hyprshot"
+		"kitty"
+		"lazygit"
+		"lsd"
+		"man-db"
+		"wget"
+		"curl"
+		"nemo"
+		"cronie"
+		"neovim"
+		"oh-my-posh"
+		"pacfiles"
+		"ripgrep"
+		"imv"
+		"dunst"
+		"zen-browser-bin"
+		"bat"
+		"bash-language-server"
+		"rofi"
+		"shellcheck"
+		"shfmt"
+		"slurp"
+		"spotify"
+		"stacer-bin"
+		"termpicker"
+		"thunderbird"
+		"tmux"
+		"ttf-jetbrains-mono-nerd"
+		"unzip"
+		"waybar"
+		"wl-clipboard"
+		"xdg-user-dirs"
+		"yq"
+		"zip"
+		"zoxide"
+		"zsa-keymapp-bin"
+		"sddm-sugar-candy-git"
+	)
 
-    for package in "${packages[@]}"; do
-        echo "Installing package: $package"
-        yay -S "$package" --noconfirm
-    done
+	for package in "${packages[@]}"; do
+		if yay -Q "$package" &>/dev/null; then
+			echo "Package '$package' is already installed, skipping..."
+		else
+			echo "Installing package: $package"
+			yay -S "$package" --noconfirm
+		fi
+	done
 }
 
 _install_nvm() {
-    if command -v nvm &>/dev/null; then
-        echo "nvm is already installed. Skipping installation."
-        return
-    fi
-    echo "Installing nvm"
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-    nvm install node
+	if [ -d "$HOME/.nvm" ]; then
+		echo "nvm is already installed. Skipping installation."
+		return
+	fi
+
+	echo "Installing nvm..."
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+	nvm install node
 }
 
 _install_rustup() {
-    if command -v rustup &>/dev/null; then
-        echo "Rustup is already installed. Skipping installation."
-        return
-    fi
+	set -e
+	if command -v rustup &>/dev/null; then
+		echo "Rustup is already installed. Skipping installation."
+		return
+	fi
 
-    echo "Installing rustup"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	echo "Installing rustup"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
 _install_tpm() {
-    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
-    tmux source "$HOME/.config/tmux/tmux.conf"
+	set -e
+	local tpm_dir="$HOME/.config/tmux/plugins/tpm"
+
+	if [ -d "$tpm_dir" ]; then
+		echo "TPM already installed. Skipping installation."
+	else
+		echo "Installing TPM..."
+		git clone https://github.com/tmux-plugins/tpm "$tpm_dir"
+	fi
+
+	tmux source "$HOME/.config/tmux/tmux.conf"
 }
 
 _install_yay
