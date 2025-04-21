d8cbc98cd60fc2f0824c9e90fcf3d84e55bca3bd
Tales Sabini <talessabini2@gmail.com>
2025-04-21 09:26:24 -0400

    updates

--- .bash_evals.sh
+++ .bash_evals.sh
@@ -3,17 +3,23 @@
 
 # oh my posh
 if command -v oh-my-posh &>/dev/null; then
-    if [ -f "$HOME/.config/ohmyposh/my_amro.toml" ]; then
-        eval "$(oh-my-posh init bash --config "$HOME"/.config/ohmyposh/my_amro.toml)"
-    else
-        if command -v brew &>/dev/null && [ -f "$(brew --prefix oh-my-posh)/themes/amro.omp.json" ]; then
-            eval "$(oh-my-posh init zsh --config "$(brew --prefix oh-my-posh)/themes/amro.omp.json")"
-        fi
-    fi
+	if [ -f "$HOME/.config/ohmyposh/my_amro.toml" ]; then
+		eval "$(oh-my-posh init bash --config "$HOME"/.config/ohmyposh/my_amro.toml)"
+	else
+		if command -v brew &>/dev/null && [ -f "$(brew --prefix oh-my-posh)/themes/amro.omp.json" ]; then
+			eval "$(oh-my-posh init zsh --config "$(brew --prefix oh-my-posh)/themes/amro.omp.json")"
+		fi
+	fi
 fi
 
-# zoxide
-eval "$(zoxide init --cmd cd bash)"
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
 # pay-respects
 eval "$(pay-respects bash --alias fk)"
+
+# proto
+eval "$(proto activate)"
+
+# zoxide
+eval "$(zoxide init bash --cmd cd)"
