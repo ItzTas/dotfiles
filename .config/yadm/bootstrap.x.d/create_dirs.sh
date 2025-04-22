f0aa983ded4c147db1e3529e88b501ab9cc31f61
Tales Sabini <talessabini2@gmail.com>
2025-04-22 17:58:43 -0400

    updates

---
+++ .config/yadm/bootstrap.x.d/create_dirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+_create() {
+    local workspace_dir
+    workspace_dir="$HOME/workspace/github.com/ItzTas"
+    mkdir -p "$workspace_dir"
+    mkdir -p "$HOME/Docs"
+    mkdir -p "$HOME/Downloads"
+    mkdir -p "$HOME/Videos"
+    mkdir -p "$HOME/Pictures"
+}
+
+_create
