ae2154961df34d145d1e924875666f49c6f51017
Tales Sabini <talessabini2@gmail.com>
2025-04-29 18:49:11 -0400

    updates

--- .config/eww/Control-Center/scripts/brightness-slider.sh
+++ .config/eww/Control-Center/scripts/brightness-slider.sh
@@ -3,14 +3,13 @@
 b=$(light -G)
 
 icon() {
-    # Lets round the float result
     bri=$(echo "($b+0.5)/1" | bc)
-    
+
     if [[ "$bri" -gt "90" ]]; then
         echo "󰃠"
-        elif [[ "$bri" -gt "50" ]]; then
+    elif [[ "$bri" -gt "50" ]]; then
         echo "󰃝"
-        elif [[ "$bri" -gt "30" ]]; then
+    elif [[ "$bri" -gt "30" ]]; then
         echo "󰃟"
     else
         echo "󰃞"
@@ -19,11 +18,11 @@ icon() {
 
 getbri() {
     bri=$(echo "($b+0.5)/1" | bc)
-    echo $bri
+    echo "$bri"
 }
 
 if [[ $1 == "--icon" ]]; then
     icon
-    elif [[ $1 == "--value" ]]; then
+elif [[ $1 == "--value" ]]; then
     getbri
 fi
---
+++ .config/hypr/custom_services/no_windows.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+CONTROL_CENTER_CONFIG_DIR="$HOME/.config/eww/Control-Center"
+
+windows_count=""
+workspace_id=""
+current_id=""
+
+_watch() {
+    while true; do
+        sleep 0.2
+        local workspace_info
+        workspace_info=$(hyprctl activeworkspace 2>/dev/null)
+
+        if [[ -z "$workspace_info" ]]; then
+            continue
+        fi
+
+        local workspace_json
+        workspace_json="$(hyprctl activeworkspace -j)"
+
+        windows_count=$(echo "$workspace_json" | jq '.windows')
+
+        workspace_id=$(echo "$workspace_json" | jq '.id')
+
+        if _has_workspace_changed; then
+            current_id="$workspace_id"
+            _kill_window
+            continue
+        fi
+        if _is_control_center_open; then
+            continue
+        fi
+        _check_workspace "$windows_count"
+    done
+}
+
+_has_workspace_changed() {
+    if [[ "$workspace_id" != "$current_id" ]]; then
+        return 0
+    fi
+    return 1
+}
+
+_is_control_center_open() {
+    eww --config "$CONTROL_CENTER_CONFIG_DIR" active-windows | grep -q "control-center"
+}
+
+_check_workspace() {
+    local windows_count="$1"
+    if ((windows_count == 0)); then
+        _on_no_windows
+        return
+    fi
+    if ((windows_count == 1)) && _is_rofi_in_active_workspace; then
+        _on_no_windows
+        return
+    fi
+    _open_waybar
+}
+
+_is_rofi_in_active_workspace() {
+    local id
+    id="$current_id"
+    hyprctl clients -j | jq -e ".[] | select(.workspace.id == $id and (.class == \"Rofi\" or .class == \"rofi\"))" >/dev/null
+}
+
+_on_windows() {
+    _kill_window
+    _open_waybar
+}
+
+_kill_window() {
+    if _is_control_center_open; then
+        eww --config "$CONTROL_CENTER_CONFIG_DIR" close control-center
+    fi
+}
+
+_is_waybar_active() {
+    if pgrep -x "waybar" >/dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+_open_waybar() {
+    if _is_waybar_active; then
+        return
+    fi
+    waybar
+}
+
+_on_no_windows() {
+    eww --config "$CONTROL_CENTER_CONFIG_DIR" open control-center
+    killall waybar
+}
+
+_watch
