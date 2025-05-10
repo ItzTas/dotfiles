cefcf4b47947ed6dd1f707fdc5a74f6b241811f8
Tales Sabini <talessabini2@gmail.com>
2025-05-10 19:08:34 -0400

    updates

--- .config/kando/scripts/fzf_dir.sh
+++ .config/kando/scripts/fzf_dir.sh
@@ -1,18 +1,32 @@
+#!/bin/env bash
+
+_handle_fifo() {
+    local fifo
+    fifo=$1
+
+    (zoxide query -l && find "$HOME" -type d) | awk '!seen[$0]++' >"$fifo" &
+}
+
 _find() {
     local dir fifo
 
     fifo=$(mktemp -u)
     mkfifo "$fifo"
 
-    (zoxide query -l && find "$HOME" -type d) >"$fifo" &
+    _handle_fifo "$fifo"
 
-    dir=$(fzf --preview 'eza -A --git --icons --tree --level 3 -F {}' --bind 'ctrl-y:accept' <"$fifo")
+    dir=$(
+        fzf \
+            --preview 'eza -A --git --icons --tree --level 3 -F {}' \
+            --bind 'ctrl-y:accept' <"$fifo"
+    )
 
     rm "$fifo"
 
     if [[ -n "$dir" ]]; then
         hyprscratch nemo "exec nemo \"$dir\""
         hyprscratch killall
+        exit 0
     fi
 }
 
