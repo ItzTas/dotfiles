af3276a705dba7c690227c4e533cfd1690b936f2
Tales <59647566+ItzTas@users.noreply.github.com>
2025-05-12 18:21:00 -0400

    updates

--- .config/yadm/bootstrap.x.d/set_up_git.sh
+++ .config/yadm/bootstrap.x.d/set_up_git.sh
@@ -1,4 +1,5 @@
 _set_up() {
+    git config --global diff.renameLimit 99999
     git config --global user.name "Tales"
     git config --global user.email "59647566+ItzTas@users.noreply.github.com"
 }
