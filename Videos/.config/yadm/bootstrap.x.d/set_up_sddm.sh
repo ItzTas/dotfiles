ffc721ca70fdd3698a34bfd045b2e0a06d1830f1
Tales <59647566+ItzTas@users.noreply.github.com>
2025-05-13 11:32:59 -0400

    updates

--- Videos/.config/yadm/bootstrap.x.d/set_up_sddm.sh
+++
@@ -1,8 +0,0 @@
-_set_up() {
-    sudo mkdir -p "/etc/sddm.conf.d"
-
-    sudo ln -s "$HOME/.config/sddm.my/theme.conf" /etc/sddm.conf.d/theme.conf
-    sudo ln -s "$HOME/.config/sddm.my/wayland.conf.conf" /etc/sddm.conf.d/wayland.conf
-}
-
-_set_up
