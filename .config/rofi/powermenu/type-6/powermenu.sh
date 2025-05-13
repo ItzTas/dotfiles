5ea55d0b8551df81608691130cfa53a477a579dc
Tales <59647566+ItzTas@users.noreply.github.com>
2025-05-13 07:00:01 -0400

    updates

--- .config/rofi/powermenu/type-6/powermenu.sh
+++ .config/rofi/powermenu/type-6/powermenu.sh
@@ -10,13 +10,25 @@
 ## style-1   style-2   style-3   style-4   style-5
 
 # Current Theme
+# HOME="${HOME:-/home/talinux}"
+#
+# if [ -f "$HOME/.profile" ]; then
+#     # shellcheck disable=SC1091
+#     source "$HOME/.profile"
+# fi
+#
+# export DISPLAY=:1
+# export XAUTHORITY="$HOME/.Xauthority"
+# export XAUTHORITY="$HOME/.Xauthority"
+# export PATH="$PATH:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin"
+
 dir="$HOME/.config/rofi/powermenu/type-6"
 theme='style-1'
 
 # CMDs
-lastlogin="`last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7`"
-uptime="`uptime -p | sed -e 's/up //g'`"
-host=`hostname`
+# lastlogin="$(last "$USER" | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7)"
+uptime="$(uptime -p | sed -e 's/up //g')"
+host=$(hostname)
 
 # Options
 hibernate=''
@@ -30,90 +42,93 @@ no=''
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -dmenu \
-		-p " $USER@$host" \
-		-mesg " Uptime: $uptime" \
-		-theme ${dir}/${theme}.rasi
+    rofi -dmenu \
+        -p " $USER@$host" \
+        -mesg " Uptime: $uptime" \
+        -theme "${dir}/${theme}.rasi"
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+        -theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+        -theme-str 'listview {columns: 2; lines: 1;}' \
+        -theme-str 'element-text {horizontal-align: 0.5;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?' \
+        -theme "${dir}/${theme}.rasi"
 }
 
 # Ask for confirmation
 confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
+    echo -e "$yes\n$no" | confirm_cmd
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
+    echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
-		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
-		elif [[ $1 == '--hibernate' ]]; then
-			systemctl hibernate
-		elif [[ $1 == '--suspend' ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $1 == '--logout' ]]; then
-			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
-				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
-				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-			elif [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
-                hyprctl dispatch exit
-			fi
-		fi
-	else
-		exit 0
-	fi
+    selected="$(confirm_exit)"
+
+    if [[ "$selected" != "$yes" ]]; then
+        exit 0
+    fi
+
+    if [[ $1 == '--shutdown' ]]; then
+        systemctl poweroff
+    elif [[ $1 == '--reboot' ]]; then
+        systemctl reboot
+    elif [[ $1 == '--hibernate' ]]; then
+        systemctl hibernate
+    elif [[ $1 == '--suspend' ]]; then
+        mpc -q pause
+        amixer set Master mute
+        systemctl suspend
+    elif [[ $1 == '--logout' ]]; then
+        if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+            openbox --exit
+        elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+            bspc quit
+        elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+            i3-msg exit
+        elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+            qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+        elif [[ "$DESKTOP_SESSION" == 'hyprland' ]]; then
+            hyprctl dispatch exit
+        fi
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $shutdown)
-		run_cmd --shutdown
-        ;;
-    $reboot)
-		run_cmd --reboot
-        ;;
-    $hibernate)
-		run_cmd --hibernate
-        ;;
-    $lock)
-		if [[ -x '/usr/bin/betterlockscreen' ]]; then
-			betterlockscreen -l
-		elif [[ -x '/usr/bin/i3lock' ]]; then
-			i3lock
-		fi
-        ;;
-    $suspend)
-		run_cmd --suspend
-        ;;
-    $logout)
-		run_cmd --logout
-        ;;
+"$shutdown")
+    run_cmd --shutdown
+    ;;
+"$reboot")
+    run_cmd --reboot
+    ;;
+"$hibernate")
+    run_cmd --hibernate
+    ;;
+"$lock")
+    if [[ -x '/usr/bin/betterlockscreen' ]]; then
+        betterlockscreen -l
+    elif [[ -x '/usr/bin/i3lock' ]]; then
+        i3lock
+    elif [[ -x '/usr/bin/hyprlock' ]]; then
+        hyprlock
+    fi
+    ;;
+"$suspend")
+    run_cmd --suspend
+    ;;
+"$logout")
+    run_cmd --logout
+    ;;
 esac
