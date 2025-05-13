32c3fe621222732829731694a15166ecc752b473
Tales <59647566+ItzTas@users.noreply.github.com>
2025-05-12 21:16:19 -0400

    updates

--- .config/hypr/scripts/volume_toggle.sh
+++ .config/hypr/scripts/volume_toggle.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-muted=$(wpctl get-mute @DEFAULT_AUDIO_SINK@)
-if [[ "$muted" == *"true"* ]]; then
-  icon="volume-mute.svg"
-  message="Audio: Muted"
+muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $3}')
+if [[ "$muted" == "[MUTED]" ]]; then
+    icon="volume-mute.svg"
+    message="Audio: Muted"
 else
-  icon="volume.svg"
-  message="Audio: Unmuted"
+    icon="volume.svg"
+    message="Audio: Unmuted"
 fi
 dunstify -i ~/.config/dunst/assets/$icon -t 500 -r 2593 "$message"
-
