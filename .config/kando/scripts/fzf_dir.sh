ceffe6c122eb570225f1509f1abc5b23c0cf4f47
Tales <59647566+ItzTas@users.noreply.github.com>
2025-06-04 17:40:18 -0400

    updates

--- .config/kando/scripts/fzf_dir.sh
+++ .config/kando/scripts/fzf_dir.sh
@@ -3,25 +3,33 @@
 _handle_fifo() {
     local fifo=$1
     local search_dir=$2
+    local mode=$3
 
-    (zoxide query -l && find "$search_dir" -type d 2>/dev/null) | awk '!seen[$0]++' >"$fifo" &
+    if [[ $mode == "file" ]]; then
+        find "$search_dir" -type f 2>/dev/null
+    else
+        zoxide query -l
+        find "$search_dir" -type d 2>/dev/null
+    fi | awk '!seen[$0]++' >"$fifo" &
 }
 
 _find() {
-    local dir fifo search_dir
+    local selected fifo search_dir mode
 
-    if [[ $1 == "root" ]]; then
-        search_dir="/"
-    else
-        search_dir="$HOME"
-    fi
+    mode="dir"
+    search_dir="$HOME"
+
+    for arg in "$@"; do
+        [[ "$arg" == "root" ]] && search_dir="/"
+        [[ "$arg" == "file" ]] && mode="file"
+    done
 
     fifo=$(mktemp -u)
     mkfifo "$fifo"
 
-    _handle_fifo "$fifo" "$search_dir"
+    _handle_fifo "$fifo" "$search_dir" "$mode"
 
-    dir=$(
+    selected=$(
         fzf \
             --preview 'eza -A --git --icons --tree --level 3 -F {}' \
             --bind 'ctrl-y:accept' <"$fifo"
@@ -29,10 +37,10 @@ _find() {
 
     rm "$fifo"
 
-    if [[ -n "$dir" ]]; then
-        hyprscratch nemo "exec nemo \"$dir\""
+    if [[ -n "$selected" ]]; then
+        hyprscratch nemo "exec nemo \"$selected\""
         exit 0
     fi
 }
 
-_find "$1"
+_find "$@"
