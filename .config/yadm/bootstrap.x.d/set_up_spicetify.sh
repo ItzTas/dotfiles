db293ee48d35d552e9f72a7fea2481d5a807d890
Tales <59647566+ItzTas@users.noreply.github.com>
2025-06-10 10:24:51 -0400

    updates

---
+++ .config/yadm/bootstrap.x.d/set_up_spicetify.sh
@@ -0,0 +1,36 @@
+_install_themes() {
+    echo ""
+    echo "installing spicetify themes"
+
+    local dest="$HOME/.config/spicetify/Themes"
+
+    local themes=(
+        "Sleek"
+    )
+
+    local tmpdir
+    tmpdir=$(mktemp -d)
+
+    git clone --depth=1 "https://github.com/spicetify/spicetify-themes" "$tmpdir"
+
+    echo "Files in the cloned output directory:"
+    ls "$tmpdir"
+
+    for theme in "${themes[@]}"; do
+        local file="$tmpdir/$theme"
+        echo "Copying file: $file"
+        cp -r "$file" "$dest"
+    done
+}
+
+_set_up() {
+    _install_themes
+
+    spicetify config current_theme Sleek color_scheme RosePine
+
+    spicetify apply
+}
+
+if command -v spicetify &>/dev/null; then
+    _set_up
+fi
