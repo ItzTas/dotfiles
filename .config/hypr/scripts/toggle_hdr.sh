abdd00c05254b57acca2b48eea13129f4ba3a0f8
Tales <59647566+ItzTas@users.noreply.github.com>
2025-06-11 19:44:14 -0400

    updates

--- .config/hypr/scripts/toggle_hdr.sh
+++ .config/hypr/scripts/toggle_hdr.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 MONITOR1="DP-2"
-HYPRMONITOR_FILE="$HOME/.config/hypr/config/hyprmonitor.conf"
+HYPRMONITOR1_FILE="$HOME/.config/hypr/config/hyprmonitor.conf"
 
 SATURATION_MONITOR1="1.45"
 BRIGHTNESS_MONITOR1="1.12"
@@ -21,17 +21,17 @@ EOF
 )
 
 _is_hdr_activated() {
-    mode=$(awk 'NR==1 { print $2 }' "$HYPRMONITOR_FILE")
+    mode=$(awk 'NR==1 { print $2 }' "$HYPRMONITOR1_FILE")
     [[ "$mode" == "1" ]]
 }
 
 _toggle() {
     if _is_hdr_activated; then
         echo "🔄 HDR is active – disabling..."
-        printf "%s\n" "$NO_HDR_TEXT" >"$HYPRMONITOR_FILE"
+        printf "%s\n" "$NO_HDR_TEXT" >"$HYPRMONITOR1_FILE"
     else
         echo "🔄 HDR is inactive – enabling..."
-        printf "%s\n" "$HDR_TEXT" >"$HYPRMONITOR_FILE"
+        printf "%s\n" "$HDR_TEXT" >"$HYPRMONITOR1_FILE"
     fi
     hyprctl reload
 }
