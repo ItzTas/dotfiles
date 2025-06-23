1b1d1c9184067cb7c5591ee0c025ee185787ed73
Tales <59647566+ItzTas@users.noreply.github.com>
2025-06-23 18:27:51 -0400

    updates

---
+++ .config/yadm/bootstrap.x.d/set_up_hyprpm.sh
@@ -0,0 +1,21 @@
+_set_up() {
+    sudo pacman -S base-devel
+    sudo pacman -S cmake meson cpio git
+
+    local plugins=(
+        "https://github.com/hyprwm/hyprland-plugins"
+    )
+
+    for plugin in "${plugins[@]}"; do
+        hyprpm add "$plugin"
+    done
+
+    local enables=(
+    )
+
+    for enable in "${enables[@]}"; do
+        hyprpm enable "$enable"
+    done
+}
+
+_set_up
