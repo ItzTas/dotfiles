7502a32eff9330bcbe6a085ca4dcc208ef0c4246
Tales <59647566+ItzTas@users.noreply.github.com>
2025-06-27 19:12:33 -0400

    updates

--- .config/yadm/bootstrap.x.d/set_up_sddm.sh
+++ .config/yadm/bootstrap.x.d/set_up_sddm.sh
@@ -1,10 +1,17 @@
 #!/bin/env bash
 
 _set_up() {
+    echo "🔧 Creating /etc/sddm.conf.d directory if it doesn't exist..."
     sudo mkdir -p "/etc/sddm.conf.d"
 
-    sudo ln -fs "$HOME/.config/sddm/theme.conf" /etc/sddm.conf.d/theme.conf
-    sudo ln -fs "$HOME/.config/sddm/wayland.conf.conf" /etc/sddm.conf.d/wayland.conf
+    echo "🔗 Creating symbolic links from ~/.config/sddm/*.conf to /etc/sddm.conf.d/"
+    for src in "$HOME/.config/sddm/"*.conf; do
+        name=$(basename "$src")
+        echo " - Linking '$src' → '/etc/sddm.conf.d/$name'"
+        sudo ln -fs "$src" "/etc/sddm.conf.d/$name"
+    done
+
+    echo "✅ Symbolic links created successfully!"
 }
 
 _set_up
