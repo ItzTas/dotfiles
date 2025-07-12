5cb40d868ebdea601fa867dd20919dbff6497619
Tales <59647566+ItzTas@users.noreply.github.com>
2025-07-12 17:02:42 -0400

    updates

--- .config/yadm/bootstrap.x.d/set_up_reflector.sh
+++ .config/yadm/bootstrap.x.d/set_up_reflector.sh
@@ -1,7 +1,14 @@
 _set_up() {
     local reflector_config_file="$HOME/.config/reflector/reflector.conf"
+    local system_config_dir="/etc/xdg/reflector"
 
-    sudo ln -sf "$reflector_config_file" "/etc/xdg/reflector/reflector.conf"
+    if [ ! -d "$system_config_dir" ]; then
+        echo "Creating directory $system_config_dir"
+        sudo mkdir -p "$system_config_dir"
+    fi
+
+    echo "Creating symlink from $reflector_config_file to /etc/xdg/reflector/reflector.conf"
+    sudo ln -fs "$reflector_config_file" "$system_config_dir/reflector.conf"
 }
 
 _set_up
