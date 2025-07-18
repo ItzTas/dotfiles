d2a0b69901d34bbcde928f1aca99cc5ffcc41aa0
Tales <59647566+ItzTas@users.noreply.github.com>
2025-07-18 16:27:17 -0400

    updates

--- .config/yadm/bootstrap.x.d/set_up_ufw.sh
+++ .config/yadm/bootstrap.x.d/set_up_ufw.sh
@@ -1,16 +1,37 @@
+#!/bin/env bash
+
 _set_up() {
     sudo ufw logging full
 
     sudo ufw default deny incoming
     sudo ufw default allow outgoing
 
-    local allows=(
-        "qBittorrent"
-    )
+    local allows=()
 
-    for allow in "${allows[@]}"; do
-        sudo ufw allow "$allow"
+    for arg in "$@"; do
+        case "$arg" in
+        --bittorrent)
+            allows+=("qBittorrent")
+            ;;
+        --localsend)
+            allows+=("53317/tcp")
+            ;;
+        *)
+            echo "Unknown argument: $arg"
+            echo "Allowed flags: 
+            --bittorrent 
+            --localsend"
+            ;;
+        esac
     done
+
+    if [ ${#allows[@]} -eq 0 ]; then
+        echo "No specific ports to allow."
+    else
+        for allow in "${allows[@]}"; do
+            sudo ufw allow "$allow"
+        done
+    fi
 
     # sudo ufw allow ssh
     # sudo ufw allow http
@@ -19,4 +40,4 @@ _set_up() {
     sudo ufw enable
 }
 
-_set_up
+_set_up "$@"
