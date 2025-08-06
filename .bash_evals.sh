4937c028d12e726b31f6f20c338ece5e3e2177ae
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-06 11:20:09 -0400

    updates

--- .bash_evals.sh
+++ .bash_evals.sh
@@ -30,3 +30,8 @@ fi
 if command -v direnv &>/dev/null; then
     eval "$(direnv hook bash)"
 fi
+
+if command -v phpenv &>/dev/null; then
+    export PATH="$HOME/.phpenv/bin:$PATH"
+    eval "$(phpenv init -)"
+fi
