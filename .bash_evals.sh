311e0abd6f7e8fb95ef30a8d6c6dce8b89374bb3
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-14 20:16:55 -0400

    updates

--- .bash_evals.sh
+++ .bash_evals.sh
@@ -1,7 +1,7 @@
 # oh my posh
 if command -v oh-my-posh &>/dev/null; then
-    if [ -f "$HOME/.config/ohmyposh/my_amro.toml" ]; then
-        eval "$(oh-my-posh init bash --config "$HOME"/.config/ohmyposh/my_amro.toml)"
+    if [ -d "$HOME/.config/ohmyposh/my_amro_colors" ]; then
+        eval "$(oh-my-posh init bash --config "$HOME"/.config/ohmyposh/my_amro_colors/my_amro_colors_2.toml)"
     else
         if command -v brew &>/dev/null && [ -f "$(brew --prefix oh-my-posh)/themes/amro.omp.json" ]; then
             eval "$(oh-my-posh init zsh --config "$(brew --prefix oh-my-posh)/themes/amro.omp.json")"
