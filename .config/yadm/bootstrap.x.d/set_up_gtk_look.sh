58d198bb277c28eef4b5810cc2beb2351687c3f8
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-14 20:46:40 -0400

    updates

---
+++ .config/yadm/bootstrap.x.d/set_up_gtk_look.sh
@@ -0,0 +1,7 @@
+_set_up() {
+    gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+    gsettings set org.gnome.desktop.interface icon-theme "dracula-icons-circle"
+    gsettings set org.gnome.desktop.interface font-name "JetBrainsMonoNL Nerd Font 10"
+}
+
+_set_up
