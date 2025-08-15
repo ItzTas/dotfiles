8b1a82f517659f8c7c8ebd6519d55486e7e59b7d
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-14 21:39:31 -0400

    updates

--- .config/hypr/scripts/volume_up.sh
+++ .config/hypr/scripts/volume_up.sh
@@ -2,5 +2,5 @@
 
 wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100)}')
-dunstify -h int:value:"$volume" -i ~/.config/dunst/assets/volume.svg -t 500 -r 2593 "Volume: $volume%"
+dunstify -a volume-change -h int:value:"$volume" -i ~/.config/dunst/assets/volume.svg -t 500 -r 2593 "Volume: $volume%"
 
