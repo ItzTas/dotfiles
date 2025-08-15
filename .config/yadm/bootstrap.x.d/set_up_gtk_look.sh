02f426df40835b118a49f325ebfef3104d23d0b8
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-15 11:03:01 -0400

    updates

--- .config/yadm/bootstrap.x.d/set_up_gtk_look.sh
+++ .config/yadm/bootstrap.x.d/set_up_gtk_look.sh
@@ -1,5 +1,5 @@
 _set_up() {
-    gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+    gsettings set org.gnome.desktop.interface gtk-theme "Dracula-alt-style"
     gsettings set org.gnome.desktop.interface icon-theme "Colorful-Dark-Icons"
     gsettings set org.gnome.desktop.interface font-name "JetBrainsMonoNL Nerd Font 10"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
