56c7090df84ee664cf3b954c3223f7edcd01f20e
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-18 21:23:10 -0400

    updates

--- .config/yadm/bootstrap.x.d/set_up_flatpak.sh
+++ .config/yadm/bootstrap.x.d/set_up_flatpak.sh
@@ -2,6 +2,7 @@ _set_up() {
     sudo flatpak override --filesystem="$HOME"/.themes
     sudo flatpak override --filesystem="$HOME"/.icons
     sudo flatpak override --filesystem=/usr/share/themes
+    sudo flatpak override --filesystem=/usr/share/icons
 }
 
 _set_up
