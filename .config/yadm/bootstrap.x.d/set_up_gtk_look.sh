64d154289b74e53e813e087f71201dc8213e78bf
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-18 21:39:54 -0400

    updates

--- .config/yadm/bootstrap.x.d/set_up_gtk_look.sh
+++ .config/yadm/bootstrap.x.d/set_up_gtk_look.sh
@@ -1,6 +1,6 @@
 _set_up() {
     gsettings set org.gnome.desktop.interface gtk-theme "catppuccin-mocha-lavender-standard+default"
-    gsettings set org.gnome.desktop.interface icon-theme "Colorful-Dark-Icons"
+    gsettings set org.gnome.desktop.interface icon-theme "candy-icons"
     gsettings set org.gnome.desktop.interface font-name "JetBrainsMonoNL Nerd Font 10"
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 }
