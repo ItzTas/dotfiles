ceb32cbb0ad1f912a758027518a70c619d6a6d94
Tales <59647566+ItzTas@users.noreply.github.com>
2025-08-28 21:21:22 -0400

    updates

---
+++ .config/hypr/scripts/spotify/volume_up.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+current=$(playerctl --player=spotify volume)
+
+new=$(awk -v v="$current" 'BEGIN { v=v+0.05; if(v>1) v=1; print v }')
+
+playerctl --player=spotify volume "$new"
+
+volume=$(awk -v v="$new" 'BEGIN { print int(v*100) }')
+
+notify-send -a "_transient" -h "int:value:$volume" -i ~/.config/dunst/assets/volume.svg -t 500 -r 2593 "Spotify: $volume%"
+
