4c9683fec6a078abe954c1cc484b33464d7d4598
Tales <59647566+ItzTas@users.noreply.github.com>
2025-09-01 21:59:13 -0400

    updates

--- .config/hypr/scripts/spotify/volume_down.sh
+++ .config/hypr/scripts/spotify/volume_down.sh
@@ -6,6 +6,7 @@ new=$(awk -v v="$current" 'BEGIN { v=v-0.05; if(v<0) v=0; print v }')
 
 playerctl --player=spotify volume "$new"
 
-volume=$(awk -v v="$new" 'BEGIN { print int(v*100) }')
+volume=$(awk -v v="$new" 'BEGIN { print int(v*100 + 0.5) }')
 
 notify-send -a "_transient" -h "int:value:$volume" -i ~/.config/dunst/assets/volume.svg -t 500 -r 2593 "Spotify: $volume%"
+
--- .config/hypr/scripts/spotify/volume_up.sh
+++ .config/hypr/scripts/spotify/volume_up.sh
@@ -6,7 +6,7 @@ new=$(awk -v v="$current" 'BEGIN { v=v+0.05; if(v>1) v=1; print v }')
 
 playerctl --player=spotify volume "$new"
 
-volume=$(awk -v v="$new" 'BEGIN { print int(v*100) }')
+volume=$(awk -v v="$new" 'BEGIN { print int(v*100 + 0.5) }')
 
 notify-send -a "_transient" -h "int:value:$volume" -i ~/.config/dunst/assets/volume.svg -t 500 -r 2593 "Spotify: $volume%"
 
