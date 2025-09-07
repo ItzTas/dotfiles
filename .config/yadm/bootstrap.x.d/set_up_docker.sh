c8cfb80f30bbe0d45111ecf7a036b17a2fd3ba35
Tales <59647566+ItzTas@users.noreply.github.com>
2025-09-07 07:28:02 -0400

    2025-09-07 07:28 - Updated: ../bootstrap,set_up_docker.sh,../../../.pacmanlist,

--- .config/yadm/bootstrap.x.d/set_up_docker.sh
+++ .config/yadm/bootstrap.x.d/set_up_docker.sh
@@ -6,8 +6,6 @@ _set_up_docker() {
     getent group docker || sudo groupadd docker
     sudo usermod -aG docker "$USER"
 
-    newgrp docker
-
     if [ -d "$HOME" ]; then
         sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
         sudo chmod g+rwx "$HOME/.docker" -R
