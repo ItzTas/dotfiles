efc985dbf2499e10d4430b6563a3a14eb9d9bccb
Tales <59647566+ItzTas@users.noreply.github.com>
2025-09-11 19:45:21 -0400

    2025-09-11 19:45 - Updated: bootstrap.x.d/install_packages.sh,

--- .config/yadm/bootstrap.x.d/install_packages.sh
+++ .config/yadm/bootstrap.x.d/install_packages.sh
@@ -259,7 +259,7 @@ _install_rustup() {
     sudo pacman -Rns rust --noconfirm
 
     echo "Installing rustup"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path
 }
 
 _install_tpm() {
