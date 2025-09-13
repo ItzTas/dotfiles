a77c2e8774c416a48304b65caa4b23054dfb56f4
Tales <59647566+ItzTas@users.noreply.github.com>
2025-09-13 04:31:34 -0400

    2025-09-13 04:31 - Updated: M .config/yadm/bootstrap.x.d/set_up/create_dirs.sh,

--- .config/yadm/bootstrap.x.d/set_up/create_dirs.sh
+++ .config/yadm/bootstrap.x.d/set_up/create_dirs.sh
@@ -2,7 +2,7 @@
 
 _create() {
     local workspace_dir
-    workspace_dir="$HOME/workspace/github.com/ItzTas"
+    workspace_dir="$HOME/Workspace/github.com/ItzTas"
     mkdir -p "$workspace_dir"
     mkdir -p "$HOME/Docs"
     mkdir -p "$HOME/Downloads"
