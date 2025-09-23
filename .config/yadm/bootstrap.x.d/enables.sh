d8b4de194b6fbe6fde467bbab26c622159927f00
Tales <59647566+ItzTas@users.noreply.github.com>
2025-09-23 18:42:28 -0400

    2025-09-23 18:42 - Updated: M .config/yadm/bootstrap.x.d/enables.sh,

--- .config/yadm/bootstrap.x.d/enables.sh
+++ .config/yadm/bootstrap.x.d/enables.sh
@@ -5,7 +5,6 @@ _start_enables() {
         "cronie"
         "docker"
         "swayosd-libinput-backend.service"
-        "clamav-daemon"
     )
     for enable in "${enables[@]}"; do
         echo "enabling service: \"$enable\""
