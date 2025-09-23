e5b2f9a2cf9b0df3f0a22a5ab24b4a99c92efcb4
Tales <59647566+ItzTas@users.noreply.github.com>
2025-09-23 11:25:02 -0400

    2025-09-23 11:25 - Updated: M ../../../hypr/config/hyprrules.conf,M ../install_packages.sh,M flatpak.sh,M ../../packagelists/flatpaklist,M ../../packagelists/pacmanlist,

--- .config/yadm/bootstrap.x.d/set_up/flatpak.sh
+++ .config/yadm/bootstrap.x.d/set_up/flatpak.sh
@@ -1,8 +1,25 @@
+_set_up_gtk_theme() {
+    mkdir -p "$HOME/.themes"
+    mkdir -p "$HOME/.icons"
+
+    sudo cp -r /usr/share/themes/catppuccin-mocha-lavender-standard+default "$HOME/.themes"
+    sudo cp -r /usr/share/icons/candy-icons "$HOME/.icons"
+
+    flatpak override --user --filesystem="$HOME/.themes"
+    flatpak override --user --filesystem="$HOME/.icons"
+
+    flatpak override --user --env=GTK_THEME=catppuccin-mocha-lavender-standard+default
+    flatpak override --user --env=ICON_THEME=candy-icons
+}
+
+_set_up_qt6_theme() {
+    flatpak override --user --env=QT_QPA_PLATFORMTHEME=qt6ct
+    flatpak override --user --filesystem="$HOME/.config/qt6ct"
+}
+
 _set_up() {
-    sudo flatpak override --filesystem="$HOME"/.themes
-    sudo flatpak override --filesystem="$HOME"/.icons
-    sudo flatpak override --filesystem=/usr/share/themes
-    sudo flatpak override --filesystem=/usr/share/icons
+    _set_up_gtk_theme
+    _set_up_qt6_theme
 }
 
 _set_up
