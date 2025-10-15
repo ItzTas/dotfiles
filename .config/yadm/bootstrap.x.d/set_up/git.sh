d901b6b76cf1d4bbe1cfc093759f944956c6a15f
Tales <59647566+ItzTas@users.noreply.github.com>
2025-10-15 19:16:41 -0400

    2025-10-15 19:16 - Updated: M git.sh,

--- .config/yadm/bootstrap.x.d/set_up/git.sh
+++ .config/yadm/bootstrap.x.d/set_up/git.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
 _set_up() {
-    git config name "Tales"
-    git config email "59647566+ItzTas@users.noreply.github.com"
+    git config --global name "Tales"
+    git config --global email "59647566+ItzTas@users.noreply.github.com"
 }
 
 _set_up
