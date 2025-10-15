87d625f717618bc62c9b7ca9bc80b3eb862ab8a0
Tales <59647566+ItzTas@users.noreply.github.com>
2025-10-14 20:28:58 -0400

    2025-10-14 20:28 - Updated: M .config/yadm/bootstrap.x.d/enables.sh,M .config/yadm/packagelists/pacmanlist,

--- .config/yadm/bootstrap.x.d/enables.sh
+++ .config/yadm/bootstrap.x.d/enables.sh
@@ -4,9 +4,16 @@ _start_enables() {
     local enables=(
         "cronie"
         "docker"
+        "minidlna"
         "swayosd-libinput-backend.service"
     )
+
     for enable in "${enables[@]}"; do
+        if ! systemctl list-unit-files | grep -q "^$enable"; then
+            echo "service \"$enable\" does not exist, skipping..."
+            continue
+        fi
+
         echo "enabling service: \"$enable\""
         sudo systemctl enable --now "$enable"
     done
