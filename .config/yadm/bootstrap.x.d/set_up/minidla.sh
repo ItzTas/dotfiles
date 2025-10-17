7787e50dc607d499ea47cfc4c4d9e62765618edb
Tales <59647566+ItzTas@users.noreply.github.com>
2025-10-16 20:52:29 -0400

    2025-10-16 20:52 - Updated: M ../yadm/bootstrap.x.d/set_up/minidla.sh,

--- .config/yadm/bootstrap.x.d/set_up/minidla.sh
+++ .config/yadm/bootstrap.x.d/set_up/minidla.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+_update_file() {
+    local conf="$HOME/.config/minidlna/minidlna.conf"
+    local target="/etc/minidlna.conf"
+
+    # Só adiciona se não existir ainda
+    if ! sudo grep -Fxq "# DOTFILES CONFIG START" "$target"; then
+        echo "→ Updating /etc/minidlna.conf with dotfiles content..."
+        {
+            echo ""
+            echo "# DOTFILES CONFIG START"
+            cat "$conf"
+            echo "# DOTFILES CONFIG END"
+        } | sudo tee -a "$target" >/dev/null
+    else
+        echo "✓ /etc/minidlna.conf already contains dotfiles config."
+    fi
+}
+
 _set_up() {
     sudo mkdir -p /opt/media/{Musics,Pictures,Videos}
 
@@ -13,6 +31,7 @@ _set_up() {
     sudo chown -R minidlna:minidlna /opt/media
     sudo chmod -R 755 /opt/media
 
+    _update_file
     echo "✓ Setup completed successfully."
 }
 
