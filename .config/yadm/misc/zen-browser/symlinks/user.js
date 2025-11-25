4a645ab6184146cfafcd1b4a871bae6e914ece96
Tales <59647566+ItzTas@users.noreply.github.com>
2025-11-25 15:57:50 -0400

    2025-11-25 15:57 - Updated: M .config/yadm/misc/zen-browser/symlinks/user.js,M .config/yadm/packagelists/pacmanlist_89568e7e669b,

--- .config/yadm/misc/zen-browser/symlinks/user.js
+++ .config/yadm/misc/zen-browser/symlinks/user.js
@@ -4,7 +4,7 @@
 user_pref('privacy.clearOnShutdown.cache', false); // Cleans cache on exit
 
 // ==== Media ====
-user_pref('media.peerconnection.enabled', false); // WebRTC (if disabled prevents IP leaks)
+user_pref('media.peerconnection.enabled', true); // WebRTC (if disabled prevents IP leaks)
 user_pref('media.peerconnection.ice.default_address_only', false); // Only use default network address for WebRTC (if enabled)
 user_pref('media.peerconnection.ice.no_host', false); // Prevents exposing local IPs in WebRTC (if enabled)
 
