3f79699fd2f933f9cd5234d9151ad57bac507992
Tales <59647566+ItzTas@users.noreply.github.com>
2025-12-08 02:34:25 -0400

    2025-12-08 02:34 - Updated: M config/hyprapps.conf,M config/hyprbinds.conf,A scripts/spotify/open_play.sh,A scripts/spotify/open_play_silent.sh,

---
+++ .config/hypr/scripts/spotify/open_play_silent.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+hyprctl dispatch exec "[workspace 9] spotify" || spotify &
+
+# while ! playerctl -p spotify status &>/dev/null; do
+#     sleep 0.05
+# done
+
+sleep 0.8
+
+playerctl -p spotify volume 0
+
+playerctl -p spotify play
