f82e358a631508e02f1a04070d12a6db28fa45a5
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-05 14:27:36 -0400

    2026-02-05 14:27 - Updated: M weston.sh,

--- .config/yadm/bootstrap.x.d/set_up/weston.sh
+++ .config/yadm/bootstrap.x.d/set_up/weston.sh
@@ -1,5 +1,5 @@
 set_up() {
-    sudo ln -sf "$HOME/.config/weston/weston.root.ini" /etc/xdg/weston/weston.ini
+    sudo cp "$HOME/.config/weston/weston.root.ini" /etc/xdg/weston/weston.ini
 }
 
 set_up
