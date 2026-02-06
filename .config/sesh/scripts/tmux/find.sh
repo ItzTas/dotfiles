26370535fb8b7cfff28ff7dc037552140afa0b92
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-06 10:53:34 -0400

    2026-02-06 10:53 - Updated: R scripts/tmux_sesh/fd_mixed.sh -> scripts/tmux/fd_mixed.sh,R scripts/tmux_sesh/find.sh -> scripts/tmux/find.sh,M ../tmux/tmux.conf,

--- .config/sesh/scripts/tmux/find.sh
+++ .config/sesh/scripts/tmux/find.sh
@@ -10,7 +10,7 @@ _find() {
             --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
             --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
             --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
-            --bind 'ctrl-f:change-prompt(🔎  )+reload(bash ~/.config/sesh/scripts/tmux_sesh/fd_mixed.sh)' \
+            --bind 'ctrl-f:change-prompt(🔎  )+reload(bash ~/.config/sesh/scripts/tmux/fd_mixed.sh)' \
             --bind 'ctrl-d:execute(chmod +x ~/.config/sesh/scripts/kill_safe.sh && ~/.config/sesh/scripts/kill_safe.sh {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
             --bind 'ctrl-i:preview-up,ctrl-u:preview-down' \
             --bind 'ctrl-y:accept' \
