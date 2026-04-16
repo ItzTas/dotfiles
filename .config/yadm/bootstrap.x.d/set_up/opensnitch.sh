5dd260f749f5dfdc88ebe9e00afbbd62c2fad124
Tales <59647566+ItzTas@users.noreply.github.com>
2026-04-16 14:48:49 -0400

    2026-04-16 14:48 - Updated: M gnome.sh,M opensnitch.sh,

--- .config/yadm/bootstrap.x.d/set_up/opensnitch.sh
+++ .config/yadm/bootstrap.x.d/set_up/opensnitch.sh
@@ -1,13 +1,5 @@
-create_symlinks() {
-    local dir = "$HOME/.config/opensnitch"
-    mkdir -p "$dir/rules"
-
-    sudo ln -s "$dir/rules" /etc/opensnitchd/rules
-}
-
 _set_up() {
     sudo systemctl enable --now opensnitchd
-    create_symlinks
 }
 
 if command -v opensnitchd &>/dev/null; then
