7d5f73f54b7da641721cda34eb21ef851f39fdb1
Tales <59647566+ItzTas@users.noreply.github.com>
2026-04-16 15:03:25 -0400

    2026-04-16 15:03 - Updated: M ../../../hypr/config/hyprservices.conf,M ../../../hypr/config/hyprservices.conf##template.esh,M firewall.sh,M ../../gnome/settings.ini,M ../../packagelists/pacman/pacmanlist_89568e7e669b,

--- .config/yadm/bootstrap.x.d/set_up/firewall.sh
+++ .config/yadm/bootstrap.x.d/set_up/firewall.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 _set_up_nftables() {
-    yes | paru -S iptables-nft
+    yes | paru -S iptables
 }
 
 _set_up_ufw() {
@@ -11,40 +11,40 @@ _set_up_ufw() {
     sudo ufw default deny incoming
     sudo ufw default allow outgoing
 
-    local allows=()
-
-    for arg in "$@"; do
-        case "$arg" in
-        --bittorrent)
-            allows+=("qBittorrent")
-            ;;
-        --localsend)
-            allows+=("53317/tcp")
-            ;;
-        --ssh)
-            allows+=("ssh")
-            ;;
-        --all)
-            allows+=("qBittorrent")
-            allows+=("ssh")
-            allows+=("53317/tcp")
-            ;;
-        *)
-            echo "Unknown argument: $arg"
-            echo "Allowed flags: 
-            --bittorrent 
-            --localsend"
-            ;;
-        esac
-    done
-
-    if [ ${#allows[@]} -eq 0 ]; then
-        echo "No specific ports to allow."
-    else
-        for allow in "${allows[@]}"; do
-            sudo ufw allow "$allow"
-        done
-    fi
+    # local allows=()
+    #
+    # for arg in "$@"; do
+    #     case "$arg" in
+    #     --bittorrent)
+    #         allows+=("qBittorrent")
+    #         ;;
+    #     --localsend)
+    #         allows+=("53317/tcp")
+    #         ;;
+    #     --ssh)
+    #         allows+=("ssh")
+    #         ;;
+    #     --all)
+    #         allows+=("qBittorrent")
+    #         allows+=("ssh")
+    #         allows+=("53317/tcp")
+    #         ;;
+    #     *)
+    #         echo "Unknown argument: $arg"
+    #         echo "Allowed flags: 
+    #         --bittorrent 
+    #         --localsend"
+    #         ;;
+    #     esac
+    # done
+    #
+    # if [ ${#allows[@]} -eq 0 ]; then
+    #     echo "No specific ports to allow."
+    # else
+    #     for allow in "${allows[@]}"; do
+    #         sudo ufw allow "$allow"
+    #     done
+    # fi
 
     sudo ufw enable
     sudo systemctl enable --now ufw
