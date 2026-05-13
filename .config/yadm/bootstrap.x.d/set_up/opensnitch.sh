158850d8d271d7ea114d1611939f679436704574
Tales <tales.gitcm@imts.aleeas.com>
2026-05-12 21:41:42 -0400

    2026-05-12 21:41 - Updated: M ../yadm/bootstrap.x.d/set_up/opensnitch.sh,

--- .config/yadm/bootstrap.x.d/set_up/opensnitch.sh
+++ .config/yadm/bootstrap.x.d/set_up/opensnitch.sh
@@ -2,6 +2,6 @@ _set_up() {
     sudo systemctl enable --now opensnitchd
 }
 
-if command -v opensnitchd &>/dev/null; then
-    _set_up
-fi
+# if command -v opensnitchd &>/dev/null; then
+#     _set_up
+# fi
