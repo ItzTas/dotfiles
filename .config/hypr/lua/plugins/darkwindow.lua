667f1b6a43942461bcc95aa49c6f54db4c4db3a1
Tales <tales.gitcm@imts.aleeas.com>
2026-05-19 08:05:43 -0400

    2026-05-19 08:05 - Updated: M lua/plugins/darkwindow.lua,M lua/settings/devices.lua,

--- .config/hypr/lua/plugins/darkwindow.lua
+++ .config/hypr/lua/plugins/darkwindow.lua
@@ -5,18 +5,18 @@ end
 hl.config({
     plugin = {
         darkwindow = {
-            -- To modify the uniforms of an already existing shader, create a new shader and set the uniforms you want
-            ["shader[tintRed]"] = {
-                from = "tint",
-                args = "tintColor=[1 0 0] tintStrength=0.1",
-            },
-
-            -- Use a custom shader from a file
-            ["shader[cool]"] = {
-                path = "/path/to/shader.glsl",
-                args = "wow=[1.0 0 0]",
-                introduces_transparency = true,
-            },
+            -- -- To modify the uniforms of an already existing shader, create a new shader and set the uniforms you want
+            -- ["shader[tintRed]"] = {
+            --     from = "tint",
+            --     args = "tintColor=[1 0 0] tintStrength=0.1",
+            -- },
+            --
+            -- -- Use a custom shader from a file
+            -- ["shader[cool]"] = {
+            --     path = "/path/to/shader.glsl",
+            --     args = "wow=[1.0 0 0]",
+            --     introduces_transparency = true,
+            -- },
         },
     },
 })
