de00093bc3e35cbcb6dc8ba9891089e8bf79bc63
Tales <tales.gitcm@imts.aleeas.com>
2026-08-21 14:30:30 -0400

    2026-08-21 14:30 - Updated: M ../../.claude/settings.json,M ../Ferdium/config/settings.json,M ../hypr/lua/system-envs.lua,A config.toml,

--- .config/hypr/lua/system-envs.lua
+++ .config/hypr/lua/system-envs.lua
@@ -6,39 +6,42 @@ local env = hl.env
 local on = hl.on
 
 on("hyprland.start", function()
-    -- cursor
-    env("XCURSOR_SIZE", "24")
-    env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
-    env("XCURSOR_THEME", "Bibata-Modern-Classic")
-    env("HYPRCURSOR_SIZE", "24")
+	-- cursor
+	env("XCURSOR_SIZE", "24")
+	env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+	env("XCURSOR_THEME", "Bibata-Modern-Classic")
+	env("HYPRCURSOR_SIZE", "24")
 
-    -- hyprland envs
-    env("XDG_SESSION_TYPE", "wayland")
-    env("XDG_CURRENT_DESKTOP", "Hyprland")
-    env("XDG_SESSION_DESKTOP", "Hyprland")
+	-- hyprland envs
+	env("XDG_SESSION_TYPE", "wayland")
+	env("XDG_CURRENT_DESKTOP", "Hyprland")
+	env("XDG_SESSION_DESKTOP", "Hyprland")
 
-    -- proton
-    env("PROTON_ENABLE_WAYLAND", "1")
-    env("PROTON_ENABLE_HDR", "1")
+	-- proton
+	env("PROTON_ENABLE_WAYLAND", "1")
+	env("PROTON_ENABLE_HDR", "1")
 
-    -- system theme
-    env("QT_QPA_PLATFORMTHEME", "qt6ct")
-    env("GTK_THEME", "catppuccin-mocha-lavender-standard+default")
+	-- system theme
+	env("QT_QPA_PLATFORMTHEME", "qt6ct")
+	env("GTK_THEME", "catppuccin-mocha-lavender-standard+default")
 
-    -- zsh home
-    env("ZDOTDIR", home .. "/.config/zsh")
+	-- zsh home
+	env("ZDOTDIR", home .. "/.config/zsh")
 
-    -- others
-    env("SSH_ASKPASS", "/usr/lib/seahorse/ssh-askpas")
+	-- others
+	env("SSH_ASKPASS", "/usr/lib/seahorse/ssh-askpas")
 
-    if gpu.is_nvidia() then
-        env("GBM_BACKEND", "nvidia-drm")
-        env("DXVK_FILTER_DEVICE_NAME", "NVIDIA GeForce RTX 4060")
-        env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-        env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+	-- Terminal
+	env("TERMINAL", "kitty")
 
-        -- nvidia hardware VA-API hardware video acceleration
-        env("LIBVA_DRIVER_NAME", "nvidia")
-        env("NVD_BACKEND", "direct")
-    end
+	if gpu.is_nvidia() then
+		env("GBM_BACKEND", "nvidia-drm")
+		env("DXVK_FILTER_DEVICE_NAME", "NVIDIA GeForce RTX 4060")
+		env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+		env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+
+		-- nvidia hardware VA-API hardware video acceleration
+		env("LIBVA_DRIVER_NAME", "nvidia")
+		env("NVD_BACKEND", "direct")
+	end
 end)
